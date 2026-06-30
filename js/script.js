9b143f595162c3e87ed378424a9978db9651001a
Vinicius <viniciusrdias@hotmail.com>
2026-06-30 17:25:54 -0300

    Novas imagens

--- js/script.js
+++ js/script.js
@@ -6,26 +6,53 @@ function inicio() {
         inicio();
     } else {
         console.log("Iniciando...")
-        return distribuirCartas(numCartas)
+        return renderizarCartas(numCartas);
     }
 }
+
+
 
 function virarCarta(carta) {
     carta.classList.toggle('flip');
 }
 
 const baralho = [
-    { id: 1, img: "onering.gif" },
-    { id: 2, img: "aragorn.gif" },
-    { id: 3, img: "frodo.gif" },
-    { id: 4, img: "gandalf.gif" },
-    { id: 5, img: "legolas.gif" },
-    { id: 6, img: "gollum.gif" },
-    { id: 7, img: "sam.gif" },
+    { id: 1, img: "imagens/sauron.gif" },
+    { id: 2, img: "imagens/aragorn.gif" },
+    { id: 3, img: "imagens/frodo.gif" },
+    { id: 4, img: "imagens/gandalf.gif" },
+    { id: 5, img: "imagens/legolas.gif" },
+    { id: 6, img: "imagens/gollum.gif" },
+    { id: 7, img: "imagens/sam.gif" },
 ]
 
+function comparador() {
+    return Math.random() - 0.5;
+}
 
+function renderizarCartas(numCartas) {
+    const cartasEmJogo = [];
+    for (let i = 0; i < numCartas / 2; i++) {
+        cartasEmJogo.push(baralho[i], baralho[i]);
+    }
 
+    cartasEmJogo.sort(comparador);
 
+    let mesa = document.querySelector(".table");
+
+    for (let i = 0; i < cartasEmJogo.length; i++) {
+        mesa.innerHTML += `
+            <div class="card" onclick="virarCarta(this)">
+            <div class="front">
+                <img src="${cartasEmJogo[i].img}" alt="">
+            </div>
+            <div class="back">
+                <img src="imagens/lotr.png" alt="">
+            </div>
+        </div>
+        `;
+    }
+
+}
 
 inicio();
