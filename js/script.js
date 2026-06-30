5a29826219028f68f8c075a666f96bdf5936f50a
Vinicius <viniciusrdias@hotmail.com>
2026-06-30 11:54:18 -0300

    feat: novos gifs e avanço na lógica

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
- function inicio() {                                                           // Define quantas cartas serão criadas
-    let qtdCartas = Number(prompt("Com quantas cartas quer jogar?"));
-    if (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 != 0 ) {
-        alert("Número de cartas inválido! Reiniciando...");
+function inicio() {
+    const numCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número par entre 4 e 14"));
+
+    if (isNaN(numCartas) || numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
+        alert("Número inválido! Digite apenas números pares de 4 a 14.");
         inicio();
+    } else {
+        console.log("Iniciando...")
+        return distribuirCartas(numCartas)
     }
-    adicionarCartas(qtdCartas);
-    
-} 
-inicio();
+}
 
 
-    function adicionarCartas(qtdCartas) {
+function virarCarta(carta) {
+    carta.classList.toggle('flip');
+}
+
+    /*function adicionarCartas(qtdCartas) {
         const mesa = document.querySelector(".table");
         for (let i = 0; i < qtdCartas ; i++) {
             mesa.innerHTML += `
                 <div class="card">
-                    <div class="imgBackCard">
+                    <div class="front">
                     <img src="imagens/lotr.png" alt="">
+                    </div>
+                    <div class="back">
+                    <img src="imagens/one_ring.gif" alt="">
                     </div>
                 </div>
                 `;
         } 
-    }
+    }*/
     
 
+inicio();
