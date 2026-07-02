1190fb0e80a408c50ffe27b429fe089e7e583838
Vinicius <viniciusrdias@hotmail.com>
2026-07-01 21:14:26 -0300

    feat: finalizado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-function inicio() {
-    const numCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número par entre 4 e 14"));
-
-    if (isNaN(numCartas) || numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
-        alert("Número inválido! Digite apenas números pares de 4 a 14.");
-        inicio();
-    } else {
-        console.log("Iniciando...")
-        return renderizarCartas(numCartas);
-    }
-}
-
-
-
-function virarCarta(carta) {
-    carta.classList.toggle('flip');
-}
-
 const baralho = [
     { id: 1, img: "imagens/sauron.gif" },
     { id: 2, img: "imagens/aragorn.gif" },
@@ -26,12 +8,31 @@ const baralho = [
     { id: 7, img: "imagens/sam.gif" },
 ]
 
+const cartasEmJogo = [];
+let cartasViradas = [];
+let acertos = 0;
+let numCartas = 0;
+let tentativas = [];
+
+
+function inicio() {
+    numCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número par entre 4 e 14"));
+
+    if (isNaN(numCartas) || numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
+        alert("Número inválido! Digite apenas números pares de 4 a 14.");
+        inicio();
+    } else {
+        console.log("Iniciando...")
+        return renderizarCartas(numCartas);
+    }
+}
+
 function comparador() {
     return Math.random() - 0.5;
 }
 
 function renderizarCartas(numCartas) {
-    const cartasEmJogo = [];
+
     for (let i = 0; i < numCartas / 2; i++) {
         cartasEmJogo.push(baralho[i], baralho[i]);
     }
@@ -52,7 +53,50 @@ function renderizarCartas(numCartas) {
         </div>
         `;
     }
-
 }
+
+
+function desvirarCartas(carta1, carta2) {
+    carta1.classList.remove("flip");
+    carta2.classList.remove("flip");
+    cartasViradas = [];
+}
+
+
+function virarCarta(elemento) {
+    if (elemento.classList.contains("flip") || cartasViradas.length === 2) {
+        return;
+    }
+
+    elemento.classList.add("flip");
+    cartasViradas.push(elemento);
+    tentativas.push(elemento);
+
+    if (cartasViradas.length === 2) {
+        const carta1 = cartasViradas[0];
+        const carta2 = cartasViradas[1];
+        const img1 = carta1.querySelector(".front img").src;
+        const img2 = carta2.querySelector(".front img").src;
+
+        if (img1 === img2) {
+            cartasViradas = [];
+            acertos = acertos + 1;
+            verificarVitoria();
+        } else {
+            setTimeout(function () {
+                desvirarCartas(carta1, carta2);
+            }, 1000);
+        }
+    }4
+}
+
+function verificarVitoria() {
+    if (acertos === numCartas / 2) {
+        setTimeout(function () {
+            alert(`Você ganhou em ${tentativas.length} jogadas!!!`);
+        }, 300);
+    }
+}
+
 
 inicio();
